274908a19d4b46060de33045ab6c022753f2be5d
brreinac <brreinac@users.noreply.github.com>
2020-02-10 00:52:01 -0500

    Solcion de errores en merge

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,10 @@
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
-import { Router } from '@angular/router';
-
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css', './css/main.component.css']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app workss!';
+  title = 'demo';
 }
